89f870aaee65b2bea41c436021457bf399ba6580
gianmarcodalfonso <gianmarcoda10.gg@gmail.com>
2025-12-15 11:06:58 +0100

    feat: contructor, getElementoSuccessivo()

---
+++ src/org/lessons/java/CiclaInteri.java
@@ -0,0 +1,19 @@
+package org.lessons.java;
+
+public class CiclaInteri {
+  private int[] numsArray;
+  private int currentIndex = 0;
+
+  // costruttore
+  public CiclaInteri(int[] numeri) {
+    this.numsArray = numeri;
+  }
+
+  public int getElementoSuccessivo() {
+    int currentElement = numsArray[currentIndex];
+    currentIndex++;
+    return currentElement;
+  }
+
+  
+}
