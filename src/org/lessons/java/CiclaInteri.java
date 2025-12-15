d81d3b1c3ad695327af8f14d825c1ea656e2e328
gianmarcodalfonso <gianmarcoda10.gg@gmail.com>
2025-12-15 11:36:53 +0100

    feat: bonus

--- src/org/lessons/java/CiclaInteri.java
+++ src/org/lessons/java/CiclaInteri.java
@@ -18,4 +18,19 @@ public class CiclaInteri {
   public boolean hasAncoraElementi() {
     return currentIndex < numsArray.length;
   }
+
+  // bonus
+
+  public CiclaInteri() {
+    this.numsArray = new int[0];
+  }
+
+  public void addElemento(int newElement){
+    int[] newArray = new int[this.numsArray.length + 1];
+    for (int i = 0; i < this.numsArray.length; i++) {
+      newArray[i] = this.numsArray[i];
+    }
+    newArray[newArray.length - 1] = newElement;
+    this.numsArray = newArray;
+  }
 }
